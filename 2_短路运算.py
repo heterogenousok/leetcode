1e20c1da8069d10253672e6e1df062f86852d804
heterogenousok <82374551+heterogenousok@users.noreply.github.com>
2021-06-14 18:57:29 +0800

    Add files via upload

---
+++ 2_短路运算.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# author luke
+
+1 ==0 and print('I am and')
+
+1 ==0 or print('I am or')
+
+print(list(range(5,-1,-1)))
+
+a=[1,2,3]
+b=a
+a[0]=3
+print(b)
+
+import time
+
+# start=time.time()
+# print(start)
+#
+# end=time.time()
